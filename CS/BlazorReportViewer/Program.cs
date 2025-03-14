d2b5c8204d4787d81d73172541a2e12e4168b46f
Polina Tyureva <polina.tyureva@devexpress.com>
2025-03-14 17:47:49 +0400

    update example

--- CS/BlazorReportViewer/Program.cs
+++ CS/BlazorReportViewer/Program.cs
@@ -21,7 +21,7 @@ IChatClient chatClient = new AzureOpenAIClient(
     new Uri(settings.AzureOpenAIEndpoint),
     new AzureKeyCredential(settings.AzureOpenAIKey)).AsChatClient(settings.DeploymentName);
 
-builder.Services.AddChatClient(config => config.Use(chatClient));
+builder.Services.AddChatClient(chatClient);
 builder.Services.AddDevExpressAI((config) => {
     config.AddBlazorReportingAIIntegration(config => {
         config.SummarizationMode = SummarizationMode.Abstractive;
